0dde953802f025e5e49b05c113e146f38cdad5dc
aether0023 <71839333+aether0023@users.noreply.github.com>
2020-10-29 18:29:24 +0300

    Update top.js

--- Commands/top.js
+++ Commands/top.js
@@ -8,7 +8,7 @@ module.exports.execute = async (client, message, args) => {
     let embed = new MessageEmbed().setTitle(message.member.displayName, message.author.avatarURL({dynamic: true})).setFooter(message.guild.name).setThumbnail(message.guild.iconURL({dynamic: true})).setColor("RANDOM").setTimestamp();
     let teyitData = rdb.get("reg") || {};
     let data = Object.keys(teyitData);
-    let dataTop = data.filter(x => message.guild.members.cache.has(x)).sort((a, b) => Number((teyitData[b].erkek || 0) + (teyitData[b].kadin)) - Number((teyitData[a].erkek) + (teyitData[a].kadin))).map((value, index) => `\`${index+1}.\` ${message.guild.members.cache.get(value)} adlı üyenin toplam **${(teyitData[value].erkek || 0) + (teyitData[value].kadin)}** (\`${teyitData[value].erkek || 0}\` erkek, \`${teyitData[value].kadin || 0}\` kadın)`).splice(0, 30).join("\n");
+    let dataTop = data.filter(x => message.guild.members.cache.has(x)).sort((a, b) => Number((teyitData[b].erkek || 0) + (teyitData[b].kadin || 0)) - Number((teyitData[a].erkek) + (teyitData[a].kadin))).map((value, index) => `\`${index+1}.\` ${message.guild.members.cache.get(value)} adlı üyenin toplam **${(teyitData[value].erkek || 0) + (teyitData[value].kadin)}** (\`${teyitData[value].erkek || 0}\` erkek, \`${teyitData[value].kadin || 0}\` kadın)`).splice(0, 30).join("\n");
     message.channel.send(embed.setDescription(`**Sıralama** \n\n ${dataTop || 'Sıralama için yeterli veri bulunamadı!'}`));
 };
 
