96ae829808b5aeef5b84d34c1ea5dcebbe208b64
aether0023 <71839333+aether0023@users.noreply.github.com>
2020-10-29 18:18:05 +0300

    Update isimler.js

--- Commands/isimler.js
+++ Commands/isimler.js
@@ -8,7 +8,7 @@ module.exports.execute = async (client, message, args) => {
     let embed = new MessageEmbed().setTitle(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor("RANDOM").setFooter("AETHER")
     let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
     let data = kdb.get(`isimler.${member.id}`);
-    let listedData = data.length > 0 ? data.map((value, index) => `\`${index + 1}.\` ${value.guildName} ismiyle \`${new Date(value.Zaman).format("DD/MM/YYYY HH:mm")}\` tarihinde ${message.guild.members.cache.has(value.Yetkili) ? message.guild.members.cache.get(value.Yetkili) : "Bulunamadı."} tarafından **${value.Komut}** olarak kaydedildi.`) : "Bu Üyenin İsim Geçmişi Bulunamadı.";
+    let listedData = data.length > 0 ? data.map((value, index) => `\`${index + 1}.\` ${value.guildName} ismiyle \`${new Date(value.Zaman).toTurkishFormatDate()}\` tarihinde ${message.guild.members.cache.has(value.Yetkili) ? message.guild.members.cache.get(value.Yetkili) : "Bulunamadı."} tarafından **${value.Komut}** olarak kaydedildi.`) : "Bu Üyenin İsim Geçmişi Bulunamadı.";
     message.channel.send(embed.setDescription(`${listedData.join("\n")}`));
 
     
